90a970001100396d2be67f0fb5421a75b67ec71c
simpsonyuki <yukilin0327@gmail.com>
2020-07-05 11:23:57 +0800

    0705-完成(精簡版)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 // 設定文件準備完成後再執行, 避免錯誤發生
 $(document).ready(function () {
-
     // 當視窗捲動時執行
     $(window).scroll(function () {
 
-        // 快捷鍵: jqeachelement
-        // $("*"):選取所有元素, 執行涵式
         $("*").each(function (index, element) {
             // element == this :表當下搜尋到的元素
 
@@ -13,33 +10,18 @@ $(document).ready(function () {
             // 設定完成之後即可直接以"變數名稱"進行套用
             var offset = $(this).offset();  // 取得元素位移資料
             var top = offset.top;    //上方位移資料
-            var left = offset.left;  //左方位移資料
+            // 位移: 元素往上加多少距離(px)開始執行, 數字越大越早執行
 
-
-            // console.log("元素的上方:"+top);
             var windowTop = $(window).scrollTop();
 
-            // console.log("視窗的高度:"+windowTop);
-
-
-            // 快捷鍵: jquattr + tab
             // 取得參數: 數字/時間/位移
             var number = $(this).attr("data-counter-number");
             var duration = $(this).attr("data-counter-duration");
             var offset = $(this).attr("data-counter-offset");
 
-            // 用 console.log 測試參數是否有被正常抓取(測試沒問題則可註解掉):
-            // console.log("數字:" + number);
-            // console.log("時間:" + duration);
-            // console.log("位移:" + offset);
-
-
             // 判斷式 : 如果 發生(指定條件) 就(執行動作)...
             // if (條件) {執行動作}
             if (windowTop > top - offset) {
-                // 用 console.log 測試判斷式是否有被正常抓取(測試沒問題則可註解掉):
-                // console.log("開始執行動作");
-
                 // $("選取").動畫({參數}, {選項})
                 $(this).animate({
                     num: number       //計數器結束的數字
